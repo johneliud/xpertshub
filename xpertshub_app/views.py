21dcd62f761d9e2b3d8fedf51ef63470741eaabb
John <johneliud4@gmail.com>
2025-12-08 21:24:16 +0300

    refactor: add explicit home redirect to logout view
    Update logout view to redirect users to the homepage after logging out, providing a clear navigation flow instead of the default Django admin logout page.

--- xpertshub_app/views.py
+++ xpertshub_app/views.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.views import LogoutView
 from django.shortcuts import render
+from django.urls import reverse_lazy
 
 # Create your views here.
 
@@ -7,4 +8,4 @@ def home(request):
     return render(request, 'xpertshub_app/home.html')
 
 class UserLogoutView(LogoutView):
-    pass
+    next_page = reverse_lazy('home')
