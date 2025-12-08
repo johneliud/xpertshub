149ae8e79e9c45e371b33155559645a5dca2dd58
John <johneliud4@gmail.com>
2025-12-08 20:46:19 +0300

    refactor: update home view to render dedicated homepage template.

--- xpertshub_app/views.py
+++ xpertshub_app/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render
 # Create your views here.
 
 def home(request):
-    return render(request, 'xpertshub_app/base.html')
+    return render(request, 'xpertshub_app/home.html')
 
 class UserLogoutView(LogoutView):
     pass
