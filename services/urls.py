e60ce852180b3756f8eaa9984be31b6968b5b3a6
John <johneliud4@gmail.com>
2025-12-09 11:46:11 +0300

    feat: add all services view URL for service browsing
    Extend services URL routing to include a listing page for customers to browse available services, establishing a complete service discovery flow.

--- services/urls.py
+++ services/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import CreateServiceView
+from .views import CreateServiceView, AllServicesView
 
 urlpatterns = [
+    path('', AllServicesView.as_view(), name='all_services'),
     path('create/', CreateServiceView.as_view(), name='create_service'),
 ]
